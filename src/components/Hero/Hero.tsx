8d9052bab9d864256bcc40b6e270ab515dac3735
Lukasz Bialy <bialy.lukasz0@gmail.com>
2023-04-24 17:47:08 +0100

    working on navbar and hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -10,17 +10,8 @@ const Hero = () => {
     <>
       <div className="hero">
         <div className="hero__content">
-          <h1 className="hero__title">Hero Title</h1>
+          <h1 className="hero__title"></h1>
           <p className="hero__description">Hero Description</p>
-        </div>
-        <div>
-          <nav>
-            <ul>
-              <li>Home</li>
-              <li>About</li>
-              <li>Contact</li>
-            </ul>
-          </nav>
         </div>
         <Button onClick={handleClick} variant='primary'>Learn More</Button>
       </div>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+// import React from 'react'
+
+import Logo from '../../assets/Pro-AccX-logo-3.png'
+
 
 const Navbar = () => {
+
+
   return (
-    <div>Navbar</div>
+    <div className='min-h-20 max-h-auto border-b-2 sticky'>
+      <div className='flex justify-between h-20 m-4 px-4 py-2 '>
+        <div className='w-1/3 h-full'>
+          <img src={Logo} alt="Pro Accx logo" className='min-w-min max-w-xs h-16 mx-auto' />
+        </div>
+        <nav className='w-2/3'>
+          <ul className='flex justify-end items-end h-full'>
+            <a href=""><li className='mx-4'>Home</li></a>
+            <a href=""><li className='mx-4'>About</li></a>
+            <a href=""><li className='mx-4'>Contact</li></a>
+          </ul>
+        </nav>
+      </div>
+    </div>
   )
 }
 
